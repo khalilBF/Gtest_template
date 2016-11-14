e2ca1a04c859e4fbbff5388c461fa9c71d91e606
Khalil <khalil.ben.fadhel.1@gmail.com>
2016-11-14 10:00:59 -0500

    Add files via upload

---
+++ gtest empty cmake project/src/some_class.cpp
@@ -0,0 +1,5 @@
+//
+// Created by khalil on 16-11-14.
+//
+
+#include "some_class.h"
---
+++ gtest empty cmake project/src/some_class.h
@@ -0,0 +1,14 @@
+//
+// Created by khalil on 16-11-14.
+//
+
+#ifndef SOME_CLASS_H
+#define SOME_CLASS_H
+
+
+class some_class {
+
+};
+
+
+#endif //GTEST_TP2_SOME_CLASS_H
